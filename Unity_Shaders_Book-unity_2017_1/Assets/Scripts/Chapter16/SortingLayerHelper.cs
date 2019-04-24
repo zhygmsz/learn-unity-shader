29f7a32e3626e27f67634ea752586fda9c340881
zhaohaoyang <652246564@qq.com>
2019-04-24 19:57:17 +0800

    验证SortingLayer的作用

---
+++ Unity_Shaders_Book-unity_2017_1/Assets/Scripts/Chapter16/SortingLayerHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortingLayerHelper : MonoBehaviour
+{
+
+    public string sortingLayerName = "";
+
+    private void Awake()
+    {
+        MeshRenderer meshRenderer = transform.GetComponent<MeshRenderer>();
+        if (meshRenderer != null)
+        {
+            meshRenderer.sortingLayerID = SortingLayer.NameToID(sortingLayerName);
+        }
+            
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
