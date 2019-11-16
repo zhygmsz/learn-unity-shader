e82b4dc5ba86d692818d15c48994feb02db1beb9
zhaohaoyang <652246564@qq.com>
2019-11-16 23:36:04 +0800

    融雪脚本

---
+++ Unity_Shaders_Book-unity_2017_1/Assets/Scripts/Learn/SnowMelt.cs
@@ -0,0 +1,213 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class SnowMelt : MonoBehaviour
+{
+    private Renderer mRenderer = null;
+    private Material mMat = null;
+
+    // 非NGUI图集方案
+    public Texture mAtlasTex;
+    // 法线只需要一张即可，选取中间帧
+    public Texture mNormalTex;
+    public int mRowNum;
+    public int mColNum;
+
+    private List<Vector4> mSpriteUVList = null;
+    private int mSpriteNum;
+
+    private int mCurTexIdx = -1;
+    private int mNextTexIdx = -1;
+
+    // 两张图片之间可插值帧数
+    [Tooltip("两片雪花之间可插值帧数")]
+    [Range(10, 20)]
+    public int mLerpMaxFrame = 15;
+    private int mCurLerpCount = -1;
+    private float mLerpStepValue = 1f;
+
+    // 雪花类型，来自mgr的赋值，用于mgr分类回收
+    private int mSnowType = -1;
+    private SnowMeltMgr.OnSnowMeltEndCallback mOnSnowMeltEndCallback;
+
+    // 测试用字段
+    // 缓存位置，在Show时不立即更新位置，在Update里更新两帧后再更新位置，尝试解决闪过问题
+    private Vector3 mCachedLocalPos;
+    // 在Show方法里先挪到远方，待Updata里更新
+    private Vector3 mFarLocalPos = new Vector3(-10000f, 0f, 0f);
+
+    private void Start()
+    {
+        Init();
+        Reset();
+    }
+
+    public void Init()
+    {
+        mRenderer = transform.GetComponent<Renderer>();
+        if (mRenderer == null)
+        {
+            string str = string.Format("SnowMelt miss Renderer, transform.name = {0}", transform.name);
+            Debug.LogError(str);
+            return;
+        }
+
+        mMat = mRenderer.material;
+        if (mMat == null)
+        {
+            string str = string.Format("mRenderer.material is null, transform.name = {0}", transform.name);
+            Debug.LogError(str);
+            return;
+        }
+
+        if (mAtlasTex && mNormalTex)
+        {
+            mMat.SetTexture("_AtlasTex", mAtlasTex);
+            mMat.SetTexture("_NormalTex", mNormalTex);
+        }
+        else
+        {
+            Debug.LogError("mAtlas or mNormalTex is null");
+            return;
+        }
+
+        CalSpriteUVList();
+
+        CalLerpStepValue();
+    }
+
+    public void SetSnowType(int snowType, SnowMeltMgr.OnSnowMeltEndCallback onSnowMeltEndCallback)
+    {
+        mSnowType = snowType;
+        mOnSnowMeltEndCallback = onSnowMeltEndCallback;
+    }
+
+    public int GetSnowType()
+    {
+        return mSnowType;
+    }
+
+    private void NotifySnowMeltEnd()
+    {
+        if (mOnSnowMeltEndCallback != null)
+        {
+            mOnSnowMeltEndCallback(this);
+        }
+    }
+
+    private void CalSpriteUVList()
+    {
+        mSpriteNum = mRowNum * mColNum;
+        if (mSpriteNum == 0)
+        {
+            string str = string.Format("mSpriteNum is 0, transform.name = {0}", transform.name);
+            Debug.LogError(str);
+            return;
+        }
+
+        mSpriteUVList = new List<Vector4>(mSpriteNum);
+
+        // sprite name强制xuehua_x格式（x从0开始自增），不是的则报错
+        int texW = mAtlasTex.width;
+        int texH = mAtlasTex.height;
+        int spriteW = texW / mColNum;
+        int spriteH = texH / mRowNum;
+        Rect rect = new Rect();
+        for (int row = 0; row < mRowNum; ++row)
+        {
+            rect.yMin = spriteH * row;
+            rect.yMax = spriteH * (row + 1);
+            for (int col = 0; col < mColNum; ++col)
+            {
+                rect.xMin = spriteW * col;
+                rect.xMax = spriteW * (col + 1);
+                Rect uv = NGUIMath.ConvertToTexCoords(rect, texW, texH);
+                mSpriteUVList.Add(new Vector4(uv.xMin, uv.xMax, uv.yMin, uv.yMax));
+            }
+        }
+    }
+
+    private void CalLerpStepValue()
+    {
+        mLerpStepValue = 1f / (mLerpMaxFrame - 1);
+    }
+
+    // inspector面板属性变动回调
+    private void OnValidate()
+    {
+        CalLerpStepValue();
+        Reset();
+    }
+
+    private void Reset()
+    {
+        mCurLerpCount = -1;
+    }
+
+    // 开始一次融化过程
+    public void Show(Vector3 localPos)
+    {
+        Reset();
+        transform.localPosition = mFarLocalPos;
+        gameObject.SetActive(true);
+
+        mCachedLocalPos = localPos;
+    }
+
+    // 隐藏go，重置逻辑，供下次显示
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+        Reset();
+    }
+
+    private void Update()
+    {
+        ++mCurLerpCount;
+        if (mCurLerpCount == 2)
+        {
+            transform.localPosition = mCachedLocalPos;
+        }
+        int curTexIdx = mCurLerpCount / mLerpMaxFrame;
+        float lerpValue = (mCurLerpCount % mLerpMaxFrame) * mLerpStepValue;
+        if (curTexIdx != mCurTexIdx)
+        {
+            mCurTexIdx = curTexIdx;
+            mNextTexIdx = mCurTexIdx + 1;
+            if (mMat != null)
+            {
+                mMat.SetVector("_CurUVRange", mSpriteUVList[mCurTexIdx]);
+                mMat.SetVector("_NextUVRange", mSpriteUVList[mNextTexIdx]);
+                mMat.SetFloat("_CurLerpNextValue", lerpValue);
+            }
+        }
+        else
+        {
+            if (mMat != null)
+            {
+                mMat.SetFloat("_CurLerpNextValue", lerpValue);
+            }
+
+            if (mCurTexIdx == mSpriteNum - 2)
+            {
+                if (Mathf.Abs(lerpValue - 1f) <= float.Epsilon)
+                {
+                    // 最后一张图的最后一插值，通知结束
+                    NotifySnowMeltEnd();
+                    //Hide();
+                    //Reset();
+                }
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (mMat != null)
+        {
+            GameObject.Destroy(mMat);
+        }
+    }
+}
---
+++ Unity_Shaders_Book-unity_2017_1/Assets/Scripts/Learn/SnowMeltMgr.cs
@@ -0,0 +1,304 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowMeltMgr : MonoBehaviour
+{
+    public delegate void OnSnowMeltEndCallback(SnowMelt snowMelt);
+    private OnSnowMeltEndCallback mOnSnowMeltEndCallback;
+
+    public List<GameObject> mSnowGoList = null;
+    // 雪花样式数量，取决于mSnowGoList数量
+    private int mSnowTypeCount = 0;
+    // 每种样式雪花预先生成实例数量
+    private int mPreGenSnowMeltNum = 3;
+    private Dictionary<int, List<SnowMelt>> mSnowMeltHideDic = null;
+    private Dictionary<int, List<SnowMelt>> mSnowMeltShowingDic = null;
+
+    // 随机出现时间间隔
+    public float mMinDuration = 1f;
+    public float mMaxDuration = 3f;
+    private float mCurDuration = 1f;
+    private float mCurCounter = 0f;
+
+    // 可出现雪花区域，NGUI坐标范围
+    private Vector2 mScreenSize;
+    // 内安全区域四边界，NGUI坐标值
+    private Vector4 mSafeAreaMin;
+    // 外安全区域四边界，NGUI坐标值
+    private Vector4 mSafeAreaMax;
+    private List<Vector4> mAreaList = null;
+
+    // 雪花在不同设备分辨率下的相对观感大小要大致相同，该值为相对于UIRoot设计分辨率的缩放系数
+    // 实例化出来的雪花Go局部缩放系数都要应用该值
+    private float mSnowLocalScaleFactor = 1f;
+
+    private void Awake()
+    {
+        UIRoot uiRoot = GameObject.Find("UI Root").GetComponent<UIRoot>();
+        if (uiRoot == null)
+        {
+            Debug.LogError("uiRoot is null");
+            return;
+        }
+
+        int screenW = Screen.width;
+        int screenH = Screen.height;
+        float logicW = uiRoot.manualWidth;
+        float logicH = uiRoot.manualHeight;
+        float ratio = (float)screenW / (float)screenH;
+        if (uiRoot.fitHeight)
+        {
+            logicW = logicH * ratio;
+            mSnowLocalScaleFactor = (float)screenH / logicH;
+        }
+        else
+        {
+            logicH = logicW / ratio;
+            mSnowLocalScaleFactor = (float)screenW / logicW;
+        }
+        mScreenSize = new Vector2(logicW, logicH);
+
+        // 可以在inspector上配置
+        Vector4 minUV = new Vector4(-0.5f, 0.5f, -0.5f, 0.5f);
+        Vector4 maxUV = new Vector4(-0.85f, 0.85f, -0.85f, 0.85f);
+        InitSafeArea(minUV, maxUV);
+
+        mOnSnowMeltEndCallback = OnSnowMeltEnd;
+
+        Init();
+    }
+
+    private void OnEnable()
+    {
+        Show();
+    }
+
+    private void OnDisable()
+    {
+        Hide();
+    }
+
+    private void Update()
+    {
+        mCurCounter += Time.deltaTime;
+        if (mCurCounter >= mCurDuration)
+        {
+            RandomGenSnow(Mathf.RoundToInt(mCurDuration));
+            mCurCounter = 0f;
+            RandomOneDuration();
+        }
+    }
+
+    // 回收雪花
+    private void OnSnowMeltEnd(SnowMelt snowMelt)
+    {
+        if (snowMelt != null)
+        {
+            snowMelt.Hide();
+
+            AddSnowMelt(mSnowMeltHideDic, snowMelt);
+            RemoveSnowMelt(mSnowMeltShowingDic, snowMelt);
+        }
+    }
+
+    private void AddSnowMelt(Dictionary<int, List<SnowMelt>> dic, SnowMelt snowMelt)
+    {
+        if (snowMelt == null)
+        {
+            return;
+        }
+        int snowType = snowMelt.GetSnowType();
+        if (dic.ContainsKey(snowType))
+        {
+            List<SnowMelt> list = dic[snowType];
+            if (list != null && list.Contains(snowMelt) == false)
+            {
+                list.Add(snowMelt);
+            }
+        }
+    }
+
+    private void RemoveSnowMelt(Dictionary<int, List<SnowMelt>> dic, SnowMelt snowMelt)
+    {
+        if (snowMelt == null)
+        {
+            return;
+        }
+        int snowType = snowMelt.GetSnowType();
+        if (dic.ContainsKey(snowType))
+        {
+            List<SnowMelt> list = dic[snowType];
+            if (list != null && list.Contains(snowMelt))
+            {
+                list.Remove(snowMelt);
+            }
+        }
+    }
+
+    // 准备雪花池
+    public void Init()
+    {
+        if (mSnowGoList == null || mSnowGoList.Count == 0)
+        {
+            Debug.LogError("mSnowGoList is null or Count is 0");
+            return;
+        }
+
+        mSnowTypeCount = mSnowGoList.Count;
+
+        // int作为key，开辟相同数量，不会引发字典扩容
+        mSnowMeltHideDic = new Dictionary<int, List<SnowMelt>>(mSnowGoList.Count);
+        mSnowMeltShowingDic = new Dictionary<int, List<SnowMelt>>(mSnowGoList.Count);
+        for (int snowGoIdx = 0, max = mSnowGoList.Count; snowGoIdx < max; ++snowGoIdx)
+        {
+            // 开辟5个容量，List几乎不用扩容
+            List<SnowMelt> list = new List<SnowMelt>(5);
+            List<SnowMelt> showingList = new List<SnowMelt>(5);
+
+            for (int snowMeltIdx = 0; snowMeltIdx < mPreGenSnowMeltNum; ++snowMeltIdx)
+            {
+                SnowMelt snowMelt = CreateSnowMelt(snowGoIdx);
+                list.Add(snowMelt);
+            }
+
+            mSnowMeltHideDic.Add(snowGoIdx, list);
+            mSnowMeltShowingDic.Add(snowGoIdx, showingList);
+        }
+    }
+
+    // 创建一个雪花
+    private SnowMelt CreateSnowMelt(int snowType)
+    {
+        SnowMelt newSnowMelt = null;
+
+        if (0 <= snowType && snowType < mSnowTypeCount)
+        {
+            GameObject snowGo = mSnowGoList[snowType];
+            Vector3 preLocalScale = snowGo.transform.localScale;
+            GameObject newGo = GameObject.Instantiate(snowGo);
+            newGo.transform.parent = transform;
+            newGo.transform.localRotation = Quaternion.identity;
+            newGo.transform.localScale = preLocalScale * mSnowLocalScaleFactor;
+            newSnowMelt = newGo.GetComponent<SnowMelt>();
+            newSnowMelt.Init();
+            newSnowMelt.SetSnowType(snowType, mOnSnowMeltEndCallback);
+            newSnowMelt.Hide();
+        }
+
+        return newSnowMelt;
+    }
+
+    // 获取一个雪花，先从池里取，没有再创建
+    private SnowMelt GenSnowMelt(int snowType)
+    {
+        SnowMelt snowMelt = null;
+
+        if (mSnowMeltHideDic.ContainsKey(snowType))
+        {
+            List<SnowMelt> list = mSnowMeltHideDic[snowType];
+            if (list != null && list.Count > 0)
+            {
+                snowMelt = list[0];
+                list.RemoveAt(0);
+            }
+            else
+            {
+                snowMelt = CreateSnowMelt(snowType);
+            }
+        }
+
+        return snowMelt;
+    }
+
+    // 开始融化
+    public void Show()
+    {
+        RandomOneDuration();
+    }
+
+    // 结束融化，回收雪花并隐藏自身，可重新开始
+    public void Hide()
+    {
+        var et = mSnowMeltShowingDic.GetEnumerator();
+        while (et.MoveNext())
+        {
+            List<SnowMelt> list = et.Current.Value;
+            if (list == null)
+            {
+                continue;
+            }
+            for (int i = 0, max = list.Count; i < max; ++i)
+            {
+                SnowMelt snowMelt = list[0];
+                list.RemoveAt(0);
+
+                snowMelt.Hide();
+                AddSnowMelt(mSnowMeltHideDic, snowMelt);
+            }
+        }
+    }
+
+    private void RandomOneDuration()
+    {
+        mCurDuration = Random.Range(mMinDuration, mMaxDuration);
+    }
+
+    // 在屏幕上随机位置创建雪花，count:数量
+    private void RandomGenSnow(int count)
+    {
+        // 强制改成1个
+        count = 1;
+        for (int i = 0; i < count; ++i)
+        {
+            // 随机雪花样式
+            int snowType = Random.Range(0, mSnowTypeCount);
+            SnowMelt snowMelt = GenSnowMelt(snowType);
+            if (snowMelt != null)
+            {
+                // 随机区域和位置
+                int listIdx = Random.Range(0, mAreaList.Count);
+                Vector4 v4 = mAreaList[listIdx];
+                float rX = Random.Range(v4.x, v4.y);
+                float rY = Random.Range(v4.z, v4.w);
+                Vector3 localPos = new Vector3(rX, rY, 0);
+
+                AddSnowMelt(mSnowMeltShowingDic, snowMelt);
+
+                snowMelt.Show(localPos);
+            }
+        }
+    }
+
+    private void InitSafeArea(Vector4 minUV, Vector4 maxUV)
+    {
+        Vector4 temp = new Vector4(mScreenSize.x, mScreenSize.x, mScreenSize.y, mScreenSize.y);
+        temp *= 0.5f;
+        mSafeAreaMin = Vector4.Scale(minUV, temp);
+        mSafeAreaMax = Vector4.Scale(maxUV, temp);
+
+        mAreaList = new List<Vector4>(8);
+
+        // 1 2 3
+        // 4   5
+        // 6 7 8
+        Vector4 a1 = new Vector4(mSafeAreaMax.x, mSafeAreaMin.x, mSafeAreaMin.w, mSafeAreaMax.w);
+        mAreaList.Add(a1);
+        Vector4 a2 = new Vector4(mSafeAreaMin.x, mSafeAreaMin.y, mSafeAreaMin.w, mSafeAreaMax.w);
+        mAreaList.Add(a2);
+        Vector4 a3 = new Vector4(mSafeAreaMin.y, mSafeAreaMax.y, mSafeAreaMin.w, mSafeAreaMax.w);
+        mAreaList.Add(a3);
+
+        Vector4 a4 = new Vector4(mSafeAreaMax.x, mSafeAreaMin.x, mSafeAreaMin.z, mSafeAreaMin.w);
+        mAreaList.Add(a4);
+        Vector4 a5 = new Vector4(mSafeAreaMin.y, mSafeAreaMax.y, mSafeAreaMin.z, mSafeAreaMin.w);
+        mAreaList.Add(a5);
+
+        Vector4 a6 = new Vector4(mSafeAreaMax.x, mSafeAreaMin.x, mSafeAreaMax.z, mSafeAreaMin.z);
+        mAreaList.Add(a6);
+        Vector4 a7 = new Vector4(mSafeAreaMin.x, mSafeAreaMin.y, mSafeAreaMax.z, mSafeAreaMin.z);
+        mAreaList.Add(a7);
+        Vector4 a8 = new Vector4(mSafeAreaMin.y, mSafeAreaMax.y, mSafeAreaMax.z, mSafeAreaMin.z);
+        mAreaList.Add(a8);
+    }
+}
