2495dfcd88d0de421f0781ba9c12d76d54686a60
zhygmsz <652246564@qq.com>
2019-11-25 00:53:13 +0800

    一个批次渲染多个雪花，并添加折射效果

--- Unity_Shaders_Book-unity_2017_1/Assets/Scripts/Learn/SnowMeltBatchedMgr.cs
+++ Unity_Shaders_Book-unity_2017_1/Assets/Scripts/Learn/SnowMeltBatchedMgr.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class SnowMeltBatchedMgr : MonoBehaviour
 {
+    // mesh
     private MeshFilter mMeshFilter;
     private Mesh mMesh;
 
+    // 图集纹理
     public Texture2D mAtlasTex;
     public int mRowNum;
     public int mColNum;
@@ -22,16 +24,41 @@ public class SnowMeltBatchedMgr : MonoBehaviour
     private int mSpriteNum;
 
     // 网格数据
-    private Vector3[] mVertices = new Vector3[4];
-    private int[] mTriangles = new int[6];
-    private Vector2[] mUV0 = new Vector2[4];
-    private Vector2[] mUV1 = new Vector2[4];
-    private Vector2[] mUV2 = new Vector2[4];
-    private Vector2[] mUV3 = new Vector2[4];
-    private Vector4[] mTangents = new Vector4[4];
-    private Vector3[] mNormals = new Vector3[4];
+    // 数量过多则内存大，过少则频繁数据腾挪，考虑在50-200之间，看平均每秒钟随机出几个雪花
+    private const int MAXSNOWNUM = 100;
+    // 当前可用的雪花索引，如果找不到则需要数据腾挪，把有效的雪花靠前，无效的雪花靠后
+    private int mNextSnowIdx = 0;
+    private Vector3[] mVertices = new Vector3[4 * MAXSNOWNUM];
+    private int[] mTriangles = new int[6 * MAXSNOWNUM];
+    private Vector2[] mUV0 = new Vector2[4 * MAXSNOWNUM];
+    private Vector2[] mUV1 = new Vector2[4 * MAXSNOWNUM];
+    private Vector2[] mUV2 = new Vector2[4 * MAXSNOWNUM];
+    private Vector2[] mUV3 = new Vector2[4 * MAXSNOWNUM];
+    private Vector4[] mTangents = new Vector4[4 * MAXSNOWNUM];
+    private Vector3[] mNormals = new Vector3[4 * MAXSNOWNUM];
 
-    
+    // 随机出现时间间隔
+    public float mMinDuration = 1f;
+    public float mMaxDuration = 3f;
+    private float mCurDuration = 1f;
+    private float mCurCounter = 0f;
+
+    // 随机区域
+    // 有效的NGUI坐标范围内，雪花能出现的大小边界，不能太靠外（被UI遮住），也不能太靠里（会遮住主角）
+    private Vector4 mMinBorder = new Vector4(-0.05f, 0.05f, -0.05f, 0.05f);
+    private Vector4 mMaxBorder = new Vector4(-0.85f, 0.85f, -0.1f, 0.1f);
+    // 可出现雪花区域，NGUI坐标范围
+    private Vector2 mScreenSize;
+    // 内安全区域四边界，NGUI坐标值
+    private Vector4 mSafeAreaMin;
+    // 外安全区域四边界，NGUI坐标值
+    private Vector4 mSafeAreaMax;
+    private List<Vector4> mAreaList = null;
+
+    // 雪花在不同设备分辨率下的相对观感大小要大致相同，该值为相对于UIRoot设计分辨率的缩放系数
+    // 创建出来的雪花尺寸，都要乘以该值算出新尺寸
+    private float mSnowLocalScaleFactor = 1f;
+
 
     private void Awake()
     {
@@ -43,59 +70,103 @@ public class SnowMeltBatchedMgr : MonoBehaviour
             return;
         }
 
+        InitPreData();
+
+        InitAreaList(mMinBorder, mMaxBorder);
+        
+        InitSpriteUVList();
+
+        InitMeshData();
+
+        // 随机出第一个雪花时间间隔
+        RandomOneDuration();
+
+        // 测试
+        mSnowWidth *= 6;
+        mSnowHeight *= 6;
+        AddSnow(Vector2.zero);
+        AddSnow(new Vector2(200, 0));
+    }
+
+    // 预结算数据
+    private void InitPreData()
+    {
+        // 计算出每个雪花的尺寸，用于填充位置
         if (mAtlasTex == null)
         {
             string str = string.Format("miss mAtlasTex, transform.name = {0}", transform.name);
             Debug.LogError(str);
             return;
         }
-
         mSnowWidth = mAtlasTex.width / mColNum;
         mSnowHeight = mAtlasTex.height / mRowNum;
 
-        Init();
-    }
-
-    private void Init()
-    {
-        CalSpriteUVList();
+        mSpriteNum = mRowNum * mColNum;
 
         // 插值相关数值
         mStepValue = 1f / mLerpTimes;
-        mTotalLerpTimes = (mSpriteNum - 1) * mLerpTimes + 1 + 1;
-
-        //构建网格
-        mMesh = new Mesh();
-        mMesh.MarkDynamic();
-        mMeshFilter.mesh = mMesh;
-
-        mTriangles[0] = 0;
-        mTriangles[1] = 2;
-        mTriangles[2] = 1;
-        mTriangles[3] = 0;
-        mTriangles[4] = 3;
-        mTriangles[5] = 2;
-
-        mUV0[0].Set(0, 1);
-        mUV0[1].Set(0, 0);
-        mUV0[2].Set(1, 0);
-        mUV0[3].Set(1, 1);
-
-        mMesh.vertices = mVertices;
-        mMesh.uv = mUV0;
-        mMesh.uv2 = mUV1;
-        mMesh.uv3 = mUV2;
-        mMesh.uv4 = mUV3;
-        mMesh.triangles = mTriangles;
-        mMesh.tangents = mTangents;
-        mMesh.normals = mNormals;
-
-        AddSnow(Vector2.zero);
+        mTotalLerpTimes = (mSpriteNum - 1) * mLerpTimes + 1;
     }
 
-    private void CalSpriteUVList()
+    private void InitAreaList(Vector4 minBorder, Vector4 maxBorder)
     {
-        mSpriteNum = mRowNum * mColNum;
+        // 先计算出屏幕上有效的NGUI坐标范围
+        UIRoot uiRoot = GameObject.Find("UI Root").GetComponent<UIRoot>();
+        if (uiRoot == null)
+        {
+            Debug.LogError("uiRoot is null");
+            return;
+        }
+        int screenW = Screen.width;
+        int screenH = Screen.height;
+        float logicW = uiRoot.manualWidth;
+        float logicH = uiRoot.manualHeight;
+        float ratio = (float)screenW / (float)screenH;
+        if (uiRoot.fitHeight)
+        {
+            logicW = logicH * ratio;
+            mSnowLocalScaleFactor = (float)screenH / logicH;
+        }
+        else
+        {
+            logicH = logicW / ratio;
+            mSnowLocalScaleFactor = (float)screenW / logicW;
+        }
+        mScreenSize = new Vector2(logicW, logicH);
+
+        // 对该范围分割成多个小区域，用于随机位置使用
+        Vector4 temp = new Vector4(mScreenSize.x, mScreenSize.x, mScreenSize.y, mScreenSize.y);
+        temp *= 0.5f;
+        mSafeAreaMin = Vector4.Scale(minBorder, temp);
+        mSafeAreaMax = Vector4.Scale(maxBorder, temp);
+
+        mAreaList = new List<Vector4>(8);
+
+        // 1 2 3
+        // 4   5
+        // 6 7 8
+        Vector4 a1 = new Vector4(mSafeAreaMax.x, mSafeAreaMin.x, mSafeAreaMin.w, mSafeAreaMax.w);
+        mAreaList.Add(a1);
+        Vector4 a2 = new Vector4(mSafeAreaMin.x, mSafeAreaMin.y, mSafeAreaMin.w, mSafeAreaMax.w);
+        mAreaList.Add(a2);
+        Vector4 a3 = new Vector4(mSafeAreaMin.y, mSafeAreaMax.y, mSafeAreaMin.w, mSafeAreaMax.w);
+        mAreaList.Add(a3);
+
+        Vector4 a4 = new Vector4(mSafeAreaMax.x, mSafeAreaMin.x, mSafeAreaMin.z, mSafeAreaMin.w);
+        mAreaList.Add(a4);
+        Vector4 a5 = new Vector4(mSafeAreaMin.y, mSafeAreaMax.y, mSafeAreaMin.z, mSafeAreaMin.w);
+        mAreaList.Add(a5);
+
+        Vector4 a6 = new Vector4(mSafeAreaMax.x, mSafeAreaMin.x, mSafeAreaMax.z, mSafeAreaMin.z);
+        mAreaList.Add(a6);
+        Vector4 a7 = new Vector4(mSafeAreaMin.x, mSafeAreaMin.y, mSafeAreaMax.z, mSafeAreaMin.z);
+        mAreaList.Add(a7);
+        Vector4 a8 = new Vector4(mSafeAreaMin.y, mSafeAreaMax.y, mSafeAreaMax.z, mSafeAreaMin.z);
+        mAreaList.Add(a8);
+    }
+
+    private void InitSpriteUVList()
+    {
         if (mSpriteNum == 0)
         {
             string str = string.Format("mSpriteNum is 0, transform.name = {0}", transform.name);
@@ -124,53 +195,172 @@ public class SnowMeltBatchedMgr : MonoBehaviour
         }
     }
 
-    private void AddSnow(Vector2 pos)
+    // 初始化网格数据
+    private void InitMeshData()
     {
-        mVertices[0].Set(pos.x - mSnowWidth / 2, pos.y + mSnowHeight / 2, 0);
-        mVertices[1].Set(pos.x - mSnowWidth / 2, pos.y - mSnowHeight / 2, 0);
-        mVertices[2].Set(pos.x + mSnowWidth / 2, pos.y - mSnowHeight / 2, 0);
-        mVertices[3].Set(pos.x + mSnowWidth / 2, pos.y + mSnowHeight / 2, 0);
+        mMesh = new Mesh();
+        mMesh.MarkDynamic();
+        mMeshFilter.mesh = mMesh;
 
-        mNormals[0].Set(Time.frameCount, Time.frameCount + mTotalLerpTimes, 1);
-        mNormals[1].Set(Time.frameCount, Time.frameCount + mTotalLerpTimes, 1);
-        mNormals[2].Set(Time.frameCount, Time.frameCount + mTotalLerpTimes, 1);
-        mNormals[3].Set(Time.frameCount, Time.frameCount + mTotalLerpTimes, 1);
+        for (int snowIdx = 0; snowIdx < MAXSNOWNUM; ++snowIdx)
+        {
+            int idx6 = 6 * snowIdx;
+            int idx4 = 4 * snowIdx;
+            mTriangles[idx6 + 0] = idx4 + 0;
+            mTriangles[idx6 + 1] = idx4 + 2;
+            mTriangles[idx6 + 2] = idx4 + 1;
+            mTriangles[idx6 + 3] = idx4 + 0;
+            mTriangles[idx6 + 4] = idx4 + 3;
+            mTriangles[idx6 + 5] = idx4 + 2;
+
+            mUV0[idx4 + 0].Set(0, 1);
+            mUV0[idx4 + 1].Set(0, 0);
+            mUV0[idx4 + 2].Set(1, 0);
+            mUV0[idx4 + 3].Set(1, 1);
+        }
+
+        mMesh.vertices = mVertices;
+        mMesh.uv = mUV0;
+        mMesh.uv2 = mUV1;
+        mMesh.uv3 = mUV2;
+        mMesh.uv4 = mUV3;
+        mMesh.triangles = mTriangles;
+        mMesh.tangents = mTangents;
+        mMesh.normals = mNormals;
     }
 
-    private void CalAtlasOffset(ref Vector3 normal, out int curIdx, out float lerpValue)
+    // 数据腾挪，有效雪花往前挪并保证既有顺序，并找出下一个有效的雪花索引
+    private int DoAdjust()
     {
-        if (Time.frameCount >= (int)normal.y)
+        // 假定数组第一个为下次有效雪花索引
+        int nextIdx = 0;
+        int snowIdx4 = 0;
+        int nextIdx4 = 0;
+        for (int snowIdx = 0; snowIdx < MAXSNOWNUM; ++snowIdx)
         {
+            snowIdx4 = 4 * snowIdx;
+            float active = mNormals[snowIdx4 + 0].z;
+            if (Mathf.Abs(active - 1f) < Mathf.Epsilon)
+            {
+                // 索引相等的，数据不用挪动
+                if (nextIdx != snowIdx)
+                {
+                    nextIdx4 = 4 * nextIdx;
+                    for (int i = 0; i < 4; ++i)
+                    {
+                        mVertices[nextIdx4 + i] = mVertices[snowIdx4 + i];
+                        mUV1[nextIdx4 + i] = mUV1[snowIdx4 + i];
+                        mUV2[nextIdx4 + i] = mUV2[snowIdx4 + i];
+                        mUV3[nextIdx4 + i] = mUV3[snowIdx4 + i];
+                        mTangents[nextIdx4 + i] = mTangents[snowIdx4 + i];
+                        mNormals[nextIdx4 + i] = mNormals[snowIdx4 + i];
+                    }
+                }
+                ++nextIdx;
+            }
+        }
+
+        // 判断有效性
+        return nextIdx;
+    }
+
+    // 往mesh里填充数据，如果空余位置不足，则启动数据腾挪
+    private void AddSnow(Vector2 pos)
+    {
+        // 检查下一个可用雪花索引是否有效
+        if (mNextSnowIdx >= MAXSNOWNUM)
+        {
+            // 数据腾挪，并重新赋值一个mNextSnowIdx
+            mNextSnowIdx = DoAdjust();
+            if (mNextSnowIdx >= MAXSNOWNUM)
+            {
+                // 调整后还是没有可用的雪花索引，则需要扩容了
+                Debug.LogError("调整后，仍然没有可用的雪花索引，网格数据需要扩容");
+                return;
+            }
+        }
+
+        // 计算数组索引
+        int idx = 4 * mNextSnowIdx;
+        mVertices[idx + 0].Set(pos.x - mSnowWidth / 2, pos.y + mSnowHeight / 2, 0);
+        mVertices[idx + 1].Set(pos.x - mSnowWidth / 2, pos.y - mSnowHeight / 2, 0);
+        mVertices[idx + 2].Set(pos.x + mSnowWidth / 2, pos.y - mSnowHeight / 2, 0);
+        mVertices[idx + 3].Set(pos.x + mSnowWidth / 2, pos.y + mSnowHeight / 2, 0);
+
+        for (int i = 0; i < 4; ++i)
+        {
+            mNormals[idx + i].Set(Time.frameCount, Time.frameCount + mTotalLerpTimes, 1);
+        }
+
+        ++mNextSnowIdx;
+    }
+
+    // 在随机位置创建一个雪花
+    private void RandomGenSnow()
+    {
+        // 随机区域和位置
+        int listIdx = Random.Range(0, mAreaList.Count);
+        Vector4 v4 = mAreaList[listIdx];
+        float rX = Random.Range(v4.x, v4.y);
+        float rY = Random.Range(v4.z, v4.w);
+        Vector3 localPos = new Vector3(rX, rY, 0);
+
+        //AddSnow(localPos);
+    }
+
+    private void CalAtlasOffset(ref Vector3 normal, out int curIdx, out float lerpValue, out float active)
+    {
+        int totalFrame = mTotalLerpTimes;
+        int pastFrame = Time.frameCount - (int)normal.x;
+        if (pastFrame >= totalFrame)
+        {
+            // 后续z=0的要在shader里去除掉
             normal.z = 0f;
             curIdx = mSpriteNum - 2;
             lerpValue = 1f;
         }
-        else if (Time.frameCount == ((int)normal.y - 1))
+        else if (pastFrame == totalFrame - 1)
         {
+            // 合法有效的最后一帧
             curIdx = mSpriteNum - 2;
             lerpValue = 1f;
         }
         else
         {
-            int pastFrame = Time.frameCount - (int)normal.x - 1;
             curIdx = pastFrame / mLerpTimes;
             lerpValue = (pastFrame % mLerpTimes) * mStepValue;
         }
+
+        active = normal.z;
     }
 
     private void FillLerpValue()
     {
-        for (int idx = 0; idx < 4; ++idx)
+        for (int snowIdx = 0; snowIdx < MAXSNOWNUM; ++snowIdx)
         {
+            // 一次计算，填充四个顶点
+            int idx = 4 * snowIdx + 0;
             int curIdx;
             float lerpValue;
-            CalAtlasOffset(ref mNormals[idx], out curIdx, out lerpValue);
+            float active;
+            CalAtlasOffset(ref mNormals[idx], out curIdx, out lerpValue, out active);
             Vector4 curUV = mSpriteUVList[curIdx];
             Vector4 nextUV = mSpriteUVList[curIdx + 1];
-            mUV1[idx].Set(curUV.x, curUV.y);
-            mUV2[idx].Set(curUV.z, curUV.w);
-            mUV3[idx].Set(lerpValue, lerpValue);
-            mTangents[idx].Set(nextUV.x, nextUV.y, nextUV.z, nextUV.w);
+            for (int i = 0; i < 4; ++i)
+            {
+                mUV1[idx + i].Set(curUV.x, curUV.y);
+                mUV2[idx + i].Set(curUV.z, curUV.w);
+                mUV3[idx + i].Set(lerpValue, lerpValue);
+                mTangents[idx + i].Set(nextUV.x, nextUV.y, nextUV.z, nextUV.w);
+            }
+            // 雪花无效
+            if (Mathf.Abs(active) < Mathf.Epsilon)
+            {
+                for (int i = 0; i < 4; ++i)
+                {
+                    mNormals[idx + i].z = 0f;
+                }
+            }
         }
 
         mMesh.vertices = mVertices;
@@ -181,16 +371,27 @@ public class SnowMeltBatchedMgr : MonoBehaviour
         mMesh.normals = mNormals;
     }
 
-    private float mTimer = 0f;
-    private float mDuration = 2f;
     private void Update()
     {
-        mTimer += Time.deltaTime;
-        if (mTimer >= mDuration)
+        mCurCounter += Time.deltaTime;
+        if (mCurCounter >= mCurDuration)
         {
-            mTimer = 0f;
+            mCurCounter = 0f;
+            RandomOneDuration();
+
+            RandomGenSnow();
         }
 
-        FillLerpValue();
+        // 当前有雪花数据，才动态插值计算
+        if (mNextSnowIdx > 0)
+        {
+            // 后续，尝试把该计算挪到shader里
+            FillLerpValue();
+        }
+    }
+
+    private void RandomOneDuration()
+    {
+        mCurDuration = Random.Range(mMinDuration, mMaxDuration);
     }
 }
--- Unity_Shaders_Book-unity_2017_1/Assets/Shaders/Learn/ScreenSnowMeltBatched.shader
+++ Unity_Shaders_Book-unity_2017_1/Assets/Shaders/Learn/ScreenSnowMeltBatched.shader
@@ -3,7 +3,10 @@ Shader "Learn/ScreenSnowMeltBatched"
 {
     Properties
     {
+        _TintColor ("Tint Color", Color) = (1, 1, 1, 1)
         _MainTex("Main Tex", 2D) = "white" {}
+        _NormalTex ("Normal", 2D) = "white" {}
+        _BumpAmt ("Bump Amt", Range(0, 1000)) = 0
     }
 
     SubShader
@@ -13,6 +16,11 @@ Shader "Learn/ScreenSnowMeltBatched"
             "IgnoreProjector" = "True"
             "Queue" = "Transparent"
             "RenderType" = "Transparent"
+        }
+
+        GrabPass
+        {
+            
         }
 
         Pass
@@ -29,9 +37,18 @@ Shader "Learn/ScreenSnowMeltBatched"
             #pragma vertex vert
             #pragma fragment frag
             #include "UnityCG.cginc"
+            #pragma multi_compile_fwdbase
+            #pragma target 3.0
 
+            uniform float4 _TintColor;
             uniform sampler2D _MainTex;
             uniform float4 _MainTex_ST;
+            uniform sampler2D _NormalTex;
+            uniform float4 _NormalTex_ST;
+            uniform float _BumpAmt;
+
+            uniform sampler2D _GrabTexture;
+            uniform float4 _GrabTexture_TexelSize;
 
             struct a2v
             {
@@ -53,6 +70,7 @@ Shader "Learn/ScreenSnowMeltBatched"
                 float2 uv3 : TEXCOORD3;
                 float3 normal : TEXCOORD4;
                 float4 tangent : TEXCOORD5;
+                float4 uvgrab : TEXCOORD6;
             };
 
             v2f vert(a2v v)
@@ -66,17 +84,26 @@ Shader "Learn/ScreenSnowMeltBatched"
                 o.uv3 = v.uv3;
                 o.normal = v.normal;
                 o.tangent = v.tangent;
+                o.uvgrab = ComputeGrabScreenPos(o.pos);
                 return o;
             }
 
             fixed4 frag(v2f i) : SV_Target
             {
+                float2 bump = UnpackNormal(tex2D(_NormalTex, i.uv0)).rg;
+                float2 offset = bump * _BumpAmt * _TintColor.a * _GrabTexture_TexelSize.xy * i.uvgrab.z;
+                i.uvgrab.xy = i.uvgrab.xy + offset;
+                i.uvgrab.xy /= i.uvgrab.w;
+                float4 grabColor = tex2D(_GrabTexture, i.uvgrab.xy);
+
                 float2 curUV = float2(lerp(i.uv1.x, i.uv1.y, i.uv0.x), lerp(i.uv2.x, i.uv2.y, i.uv0.y));
                 float4 curColor = tex2D(_MainTex, curUV);
                 float2 nextUV = float2(lerp(i.tangent.x, i.tangent.y, i.uv0.x), lerp(i.tangent.z, i.tangent.w, i.uv0.y));
                 float4 nextColor = tex2D(_MainTex, nextUV);
                 float4 finalColor = lerp(curColor, nextColor, i.uv3.x);
-                return finalColor;
+
+                return lerp(grabColor, finalColor, finalColor.a);
+                //return finalColor;
             }
 
             ENDCG
